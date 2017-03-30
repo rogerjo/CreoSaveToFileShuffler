3d34488daaf10ee72904d8f6716f521060f26be3
rogerjo <rogerjoensson@outlook.com>
2017-03-30 13:37:17 +0200

    Working final release 1

--- CreoSaveToFileShuffler/CreoSaveToFileShuffler/MainWindow.xaml.vb
+++ CreoSaveToFileShuffler/CreoSaveToFileShuffler/MainWindow.xaml.vb
@@ -14,20 +14,47 @@ Class MainWindow
 
     Dim FileNameComplete As String
 
+    Sub Creo_Connect()
 
+        Dim asyncConnection As IpfcAsyncConnection = Nothing
+
+        Try
+            myInfo.Text = "Connecting..."
+
+            asyncConnection = (New CCpfcAsyncConnection).Connect(Nothing, Nothing, Nothing, Nothing)
+            session = asyncConnection.Session
+            activeserver = session.GetActiveServer
+            model = session.CurrentModel
+            myInfo.Text = "Connection established"
+
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString + Chr(13) + ex.StackTrace.ToString)
+            If Not asyncConnection Is Nothing AndAlso asyncConnection.IsRunning Then
+                asyncConnection.Disconnect(1)
+            End If
+            myInfo.Text = "Error occurred while connecting"
+
+        End Try
+    End Sub
     Private Sub MyWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles myWindow.Loaded
         myInfo.Text = "Working..."
+        Call Creo_Connect()
+
         Call SaveObjectToDisk()
     End Sub
 
     Private Sub SaveObjectToDisk()
 
-        Try
-            asyncConnection = (New CCpfcAsyncConnection).Connect(Nothing, Nothing, Nothing, Nothing)
-            session = asyncConnection.Session
-            activeserver = session.GetActiveServer
-            model = session.CurrentModel
+        'Try
+        '    asyncConnection = (New CCpfcAsyncConnection).Connect(Nothing, Nothing, Nothing, Nothing)
+        'session = asyncConnection.Session
+        'activeserver = session.GetActiveServer
+        'model = session.CurrentModel
+        'Catch ex As Exception
+        '    myInfo.Text = "No session"
+        'End Try
 
+        Try
             If model Is Nothing Then
                 MsgBox("Model is not present",, "Script message")
                 asyncConnection.Disconnect(1)
@@ -71,17 +98,20 @@ Class MainWindow
             FileNameComplete = model.FullName + "_" + model.Revision + "_" + model.Version + "_" + State + FileEnd
 
             Call ExportFileToDisc(FileNameComplete, model.Type)
-
-
         Catch ex As Exception
-            myInfo.Text = "No session"
+
         End Try
+
     End Sub
 
     Sub TestForDir(workdir As String)
-        If Dir(workdir, vbDirectory) = "" Then
-            MkDir(workdir)
-        End If
+        Try
+            If Dir(workdir, vbDirectory) = "" Then
+                MkDir(workdir)
+            End If
+        Catch ex As Exception
+
+        End Try
     End Sub
 
     Private Sub ExportFileToDisc(FileNameComplete As String, ConvertType As Integer)
@@ -89,49 +119,58 @@ Class MainWindow
         Dim Destination As String = Workdir & FileNameComplete
 
         TestForDir(Workdir)
+
         myInfo.Text = FileNameComplete.ToString()
 
 
-        If (ConvertType = 0 Or ConvertType = 1) Then 'Export assy and model to STEP
-            Dim cDesExStep As CCpfcSTEP3DExportInstructions
-            Dim DesFlags As IpfcGeometryFlags
-            Dim Des3DEx As IpfcExport3DInstructions
-            Dim DesEx As IpfcExportInstructions
-            Dim DesExStep As IpfcSTEP3DExportInstructions
+        Try
+            If (ConvertType = 0 Or ConvertType = 1) Then 'Export assy and model to STEP
+                Dim cDesExStep As CCpfcSTEP3DExportInstructions
+                Dim DesFlags As IpfcGeometryFlags
+                Dim Des3DEx As IpfcExport3DInstructions
+                Dim DesEx As IpfcExportInstructions
+                Dim DesExStep As IpfcSTEP3DExportInstructions
 
-            cDesExStep = New CCpfcSTEP3DExportInstructions
-            DesFlags = (New CCpfcGeometryFlags).Create()
-            DesFlags.AsSolids = True
-            DesExStep = cDesExStep.Create(EpfcAssemblyConfiguration.EpfcEXPORT_ASM_FLAT_FILE, DesFlags)
-            Des3DEx = DesExStep
-            DesEx = Des3DEx
+                cDesExStep = New CCpfcSTEP3DExportInstructions
+                DesFlags = (New CCpfcGeometryFlags).Create()
+                DesFlags.AsSolids = True
+                DesExStep = cDesExStep.Create(EpfcAssemblyConfiguration.EpfcEXPORT_ASM_FLAT_FILE, DesFlags)
+                Des3DEx = DesExStep
+                DesEx = Des3DEx
 
-            session.CurrentModel.Export(Destination, Des3DEx)
-
-
-        ElseIf (ConvertType = 2) Then 'Export drawing to PDF
-            Dim expdf As IpfcPDFExportInstructions
-            Dim pdfopt As IpfcPDFOption
-            Dim EpfcPDFOPT_LAUNCH_VIEWER As Boolean
-            Dim Drawing As IpfcModel2D
-            Dim sheet As IpfcSheetOwner
-
-            Drawing = CType(session.CurrentModel, IpfcModel2D)
-            Drawing.Regenerate()
+                session.CurrentModel.Export(Destination, Des3DEx)
 
 
-            EpfcPDFOPT_LAUNCH_VIEWER = True
-            expdf = (New CCpfcPDFExportInstructions).Create()
-            pdfopt = (New CCpfcPDFOption).Create()
-            pdfopt.OptionValue = (New CMpfcArgument).CreateBoolArgValue(EpfcPDFOPT_LAUNCH_VIEWER)
+            ElseIf (ConvertType = 2) Then 'Export drawing to PDF
+                Dim expdf As IpfcPDFExportInstructions
+                Dim pdfopt As IpfcPDFOption
+                Dim EpfcPDFOPT_LAUNCH_VIEWER As Boolean
+                Dim Drawing As IpfcModel2D
+                Dim sheet As IpfcSheetOwner
 
-            session.CurrentModel.Export(Destination, CType(expdf, IpfcExportInstructions))
+                Drawing = CType(session.CurrentModel, IpfcModel2D)
+                Drawing.Regenerate()
 
-        End If
+
+                EpfcPDFOPT_LAUNCH_VIEWER = True
+                expdf = (New CCpfcPDFExportInstructions).Create()
+                pdfopt = (New CCpfcPDFOption).Create()
+                pdfopt.OptionValue = (New CMpfcArgument).CreateBoolArgValue(EpfcPDFOPT_LAUNCH_VIEWER)
+
+                session.CurrentModel.Export(Destination, CType(expdf, IpfcExportInstructions))
+
+            End If
+        Catch ex As Exception
+
+        End Try
     End Sub
 
     Private Sub MyButton_Click(sender As Object, e As RoutedEventArgs) Handles myButton.Click
-        asyncConnection.Disconnect(1)
+        Try
+            asyncConnection.Disconnect(1)
+        Catch ex As Exception
+
+        End Try
         Me.Close()
     End Sub
 End Class
